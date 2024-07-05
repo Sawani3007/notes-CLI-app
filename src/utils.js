12151491a74d2cc3a08851bdc43f325f47d6a7f6
SAWANI SAXENA <66546236+Sawani3007@users.noreply.github.com>
2024-07-05 23:22:18 +0530

    Add files via upload

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+export const listNotes = (notes) => {
+  notes.forEach((note) => {
+    console.log("id: ", note.id);
+    console.log("tags: ", note.tags);
+    console.log("content: ", note.content);
+    console.log("\n");
+  });
+};
